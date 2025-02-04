4e056d86d75860866edde7cf5ec865cf6ab57bac
Madina-Web-Developer <developermadina761@gmail.com>
2025-02-05 01:40:32 +0600

    design added

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,11 +1,51 @@
 import React from 'react'
 
 const Home = () => {
+
+
   return (
+
     <div>
-      <h1 className=''>HIIIII</h1>
+
+     <nav className=' bg-amber-200 p-2'>
+      <div className="container">
+
+      <div className="b_div flex gap-6">
+
+      <button>All</button>
+      <button>Lunch</button>
+      <button>Dinner</button>
+      <button>Breakfast</button>
+      </div>
+
+      </div>
+     </nav>
+
+     {/* ------------- Nav End */}
+
+     <div className="container">
+
+      <div className="S_card_main">
+        
+      <div className="singleCard w-[300px] bg-gray-300">
+        <img src="" alt="img" />
+
+        <div className="card_data">
+          <h1>Food Name</h1>
+          <p>discription</p>
+          <h3>Price</h3>
+          <h4>discount</h4>
+          <h5>Catagory</h5>
+        </div>
+      </div>
+
+      </div>
+
+
+     </div>
     </div>
   )
 }
 
 export default Home
+
