9a4e4828bdb33c91e27f4ac118fb65c56f12dc90
Madina-Web-Developer <developermadina761@gmail.com>
2025-02-05 02:21:23 +0600

    add map

--- src/Components/Layouts/LayoutOne.jsx
+++ src/Components/Layouts/LayoutOne.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router'
-import Navbar from '../Navbar/Navbar'
+
 
 const LayoutOne = () => {
   return (
     <div>
-      <Navbar/>
+     
       <Outlet/>
     </div>
   )
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
 
+  // ---------Custom variable
+
+  const [data, Setdata] = useState([])
+
+  const [catagory, Setcatagory] = useState([])
+
+
+  // ------------ Function
+
+
+  useEffect(()=>{
+
+    fetch('https://api.jsonbin.io/v3/b/67a25305acd3cb34a8d8444c')
+    .then((response) => response.json())
+    .then((json) =>{Setdata(json.record),Setcatagory(json.record)})
+
+  },[])
+
+  console.log(catagory)
 
   return (
 
     <div>
 
-     <nav className=' bg-amber-200 p-2'>
       <div className="container">
+     <nav className='  p-2'>
 
-      <div className="b_div flex gap-6">
+      <div className="b_div flex gap-6 text-blue-500 text-[25px]">
 
       <button>All</button>
       <button>Lunch</button>
@@ -18,31 +37,41 @@ const Home = () => {
       <button>Breakfast</button>
       </div>
 
-      </div>
+      
      </nav>
 
      {/* ------------- Nav End */}
 
-     <div className="container">
+   
 
-      <div className="S_card_main">
-        
-      <div className="singleCard w-[300px] bg-gray-300">
-        <img src="" alt="img" />
+      <div className="S_card_main mt-[100px] flex flex-wrap gap-5">
 
-        <div className="card_data">
-          <h1>Food Name</h1>
-          <p>discription</p>
-          <h3>Price</h3>
-          <h4>discount</h4>
-          <h5>Catagory</h5>
+        {
+
+          data.map((item)=>(
+
+      <div key={item.Id} className="singleCard w-[280px] bg-gray-100 flex">
+       
+       <div className="Img_div w-[150px]">
+        <img src={item.Img} alt="" />
+       </div>
+
+        <div className="card_data p-[15px]">
+          <h1>{item.Name}</h1>
+          <h3>Price: {item.Price}</h3>
+          <h4>{item.DiscountPrice}</h4>
+          <h5>Catagory: {item.Category}</h5>
         </div>
       </div>
 
+          ))
+        }
+
+      
       </div>
 
 
-     </div>
+      </div>
     </div>
   )
 }
